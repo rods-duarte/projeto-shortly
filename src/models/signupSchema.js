95706fbec668f80582928f4fcd37fdd03b1d5c1b
Rodrigo Duarte Maciel <rods.dduarte@gmail.com>
2022-06-07 20:35:31 -0300

    Feat: Add signup schema

---
+++ src/models/signupSchema.js
@@ -0,0 +1,10 @@
+import Joi from 'joi';
+
+const SignUpSchema = Joi.object({
+  name: Joi().string().required(),
+  email: Joi().email().required(),
+  password: Joi().string().min(3).required(),
+  confirmPassword: Joi().ref('password'),
+}).with('confirmPassword', 'password');
+
+export default SignUpSchema;
