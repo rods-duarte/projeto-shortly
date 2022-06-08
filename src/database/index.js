5b96b475c4542da28c2e640e9586e99f8983559b
Rodrigo Duarte Maciel <rods.dduarte@gmail.com>
2022-06-07 22:42:16 -0300

    Refactor: connection config

--- src/database/index.js
+++ src/database/index.js
@@ -3,20 +3,10 @@ import dotenv from 'dotenv';
 
 dotenv.config({ path: './src/config/.env' });
 
-const user = process.env.USERNAME;
-const password = process.env.PASSWORD;
-const host = process.env.HOST;
-const port = process.env.DB_PORT;
-const database = process.env.DBNAME;
-
 const { Pool } = pg;
 
 const db = new Pool({
-  user,
-  password,
-  host,
-  port,
-  database,
+  connectionString: process.env.DATABASE_URL,
 });
 
 export default db;
