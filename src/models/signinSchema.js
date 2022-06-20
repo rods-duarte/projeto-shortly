6184a7a68404a9300650f19066f1cc27c00dd242
Guga <contato.gustavobarbosa@outlook.com>
2022-06-20 15:45:59 -0300

    Preparando revisão de código

--- src/models/signinSchema.js
+++
@@ -1,8 +0,0 @@
-import Joi from 'joi';
-
-const SignInSchema = Joi.object({
-  email: Joi.string().email().required(),
-  password: Joi.string().required(),
-});
-
-export default SignInSchema;
--- src/models/urlSchema.js
+++
@@ -1,7 +0,0 @@
-import Joi from 'joi';
-
-const UrlSchema = Joi.object({
-  url: Joi.string().uri().required(),
-});
-
-export default UrlSchema;
